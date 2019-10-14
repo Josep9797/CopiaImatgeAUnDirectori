b30ab878d1a2b5bac0db808063d930eac6db58ae
Josep9797 <55413008+Josep9797@users.noreply.github.com>
2019-10-15 00:27:28 +0200

    Add files via upload

---
+++ BuscaImg.java
@@ -0,0 +1,98 @@
+
+package CopiaImatgesDesDunDiectori;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class BuscaImg {
+    
+    
+    
+    public static void main(String[] args) {
+
+        String[] ex = {"png","img","jpg","gif"};
+        
+        Path dir = Paths.get("C:\\Users\\Josep Serra\\Desktop\\Proba");
+
+        System.out.println("Fitxers del directori " + dir);
+
+        if (Files.isDirectory(dir)) {
+            Buscar(dir);
+
+        } else {
+            System.err.println("Ús: java LlistarDirectori <directori>");
+        }
+    }
+
+    public static void Buscar(Path dir){
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir);) {
+            for (Path fitxer : stream) {
+
+                if (Files.isDirectory(fitxer)) {
+                    Buscar(fitxer);
+                }else{
+                    
+                    String extension = "";
+                    
+                    String nomFitxer = fitxer.getFileName().toString();
+                    if(nomFitxer.endsWith(".jpg")){
+                        extension = ".jpg";
+                    }else if(nomFitxer.endsWith(".png")){
+                        extension = ".png";
+                    }else if(nomFitxer.endsWith(".gif")){
+                        extension = ".gif";
+                    }else if(nomFitxer.endsWith(".bmp")){
+                        extension = ".bmp";
+                    }
+                    
+                    if(!extension.equals("")){
+                        Copiar(fitxer.toFile(), extension);
+                    }
+                }
+
+            }
+        } catch (IOException | DirectoryIteratorException ex) {
+            System.err.println(ex);
+        }
+    }
+    
+    static int numFoto = 0;
+    public static void Copiar(File ruta, String extension) throws FileNotFoundException, IOException{
+        
+        //int num = (int) (Math.random() * 10);
+                
+        FileInputStream fis = new FileInputStream(ruta);
+        
+        File fitxerDesti = new File("C:\\Users\\Josep Serra\\Desktop\\Fotos\\"+numFoto+extension);
+        FileOutputStream out = new FileOutputStream(fitxerDesti);
+        
+        byte[] blockBytes = new byte[100];
+
+        int character = fis.read();
+        int i=0;
+        while (character!=-1) {
+            if (i < 100) {
+                blockBytes[i] = (byte) character;
+
+                character = fis.read();
+                i++;
+            } else {
+                out.write(blockBytes);
+                i = 0;
+            }
+        }
+
+        if (i>0) {
+            out.write(blockBytes,0,i);
+        }
+        
+        numFoto++;
+    }
+}
